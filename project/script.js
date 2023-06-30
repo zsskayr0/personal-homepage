34af9a6b9b401bf41f11f238ce41d2a44951b93a
Diogo M. Roque <roque.dmr@outlook.com>
2023-06-30 20:02:56 -0300

    update files

--- project/script.js
+++ project/script.js
@@ -40,3 +40,62 @@ function getLocation() {
 
 // Chamada da função para obter a temperatura atual pela localização
 getLocation();
+
+// Obtendo referência para o elemento <h1> com a classe "greeting"
+const greetingElement = document.querySelector('.greeting');
+
+// Função para obter o horário atual em Brasília
+function getCurrentTime() {
+  const date = new Date();
+  const options = { timeZone: 'America/Sao_Paulo', hour: 'numeric' };
+  return date.toLocaleTimeString('en-US', options);
+}
+
+// Função para exibir a saudação apropriada com base no horário
+function displayGreeting() {
+  const currentTime = getCurrentTime();
+
+  if (currentTime >= 6 && currentTime < 12) {
+    greetingElement.textContent = 'Good morning Diogo,';
+  } else if (currentTime >= 12 && currentTime < 18) {
+    greetingElement.textContent = 'Good afternoon Diogo,';
+  } else {
+    greetingElement.textContent = 'Good evening Diogo,';
+  }
+}
+
+// Chamada da função para exibir a saudação atual
+displayGreeting();
+
+const searchInput = document.getElementById('search-input');
+const resultsList = document.getElementById('results-list');
+const websites = [
+  { name: 'Google', url: 'https://www.google.com' },
+  { name: 'YouTube', url: 'https://www.youtube.com' },
+  { name: 'Facebook', url: 'https://www.facebook.com' },
+  // Adicione mais sites ao array conforme necessário
+];
+
+searchInput.addEventListener('input', handleSearch);
+
+function handleSearch() {
+  const searchTerm = searchInput.value.toLowerCase();
+  const filteredWebsites = websites.filter(website =>
+    website.name.toLowerCase().includes(searchTerm)
+  );
+
+  displayResults(filteredWebsites);
+}
+
+function displayResults(filteredWebsites) {
+  resultsList.innerHTML = '';
+
+  filteredWebsites.forEach(website => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = website.url;
+    a.textContent = website.name;
+    li.appendChild(a);
+    resultsList.appendChild(li);
+  });
+}
